9273738357cdf6952b5599581878f57c087433d6
duckies <voducminh39@gmail.com>
2024-03-27 09:31:55 +0700

    feature: basic flow 90%

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-
-
+const btn__load = document.querySelector('#btn__load');
+const score = document.querySelector('.score');
 const grid_size = 20;
 let snakeBody = [];
 let arr_food = [];
+let snake_length = 2;
+var direction = "";
+
+
 for (let i = 1; i < 30; i++) {
     ctx.beginPath();
     ctx.moveTo(i * grid_size, 0);
@@ -20,6 +24,59 @@ for (let i = 1; i < 30; i++) {
     ctx.strokeStyle = "#1D1D2B";
     ctx.stroke();
 }
+
+let point = 0;
+let x_food, y_food;
+const makeFood = function () {
+    // Tạo một mồi mới ngẫu nhiên
+    x_food = Math.floor(Math.random() * (canvas.width / grid_size - 1)) + 1;
+    y_food = Math.floor(Math.random() * (canvas.width / grid_size - 1)) + 1;
+
+    // Kiểm tra xem mồi mới có nằm trong thân rắn hay không
+    if (snakeBody.some(function (item) {
+        return item[0] == x_food * grid_size && item[1] == y_food * grid_size;
+    })) {
+        // Nếu có, gọi lại hàm makeFood() để tạo mồi mới khác
+        makeFood();
+    } else {
+        // Nếu không, vẽ mồi mới lên canvas
+        ctx.beginPath();
+        ctx.rect(x_food * grid_size, y_food * grid_size, grid_size, grid_size);
+        ctx.fillStyle = "white";
+        ctx.fill();
+    }
+}
+makeFood();
+const eatFood = function () {
+    let head = snakeBody[snakeBody.length - 1];
+
+    if (head[0] == x_food * grid_size && head[1] == y_food * grid_size) {
+        snake_length += 1;
+        point++;
+        score.innerHTML = point;
+        makeFood();
+        // snakeBody.push([x_food,y_food]);
+    }
+}
+
+//tu. sat'
+const suicide = function () {
+    let head = snakeBody[snakeBody.length - 1];
+    for (let i = 0; i < snakeBody.length - 2; i++) {
+        item = snakeBody[i];
+        if (head[0] == item[0] && head[1] == item[1]) {
+            gameOver();
+        }
+    }
+}
+
+function gameOver() {
+    clearInterval(interval);
+    alert('game over');
+}
+
+
+// main
 class Snake {
     constructor(xpos, ypos) {
         this.xpos = xpos;
@@ -28,23 +85,23 @@ class Snake {
     }
     draw(ctx) {
         snakeBody.push([this.xpos, this.ypos]);
-        if (snakeBody.length > 2) {
+        if (snakeBody.length > snake_length) {
             var itemRemove = snakeBody.shift();//tra ve gia tri vua dc xoa'
             ctx.clearRect(itemRemove[0], itemRemove[1], this.grid_size, this.grid_size);
         }
-
+        eatFood();
+        suicide();
         ctx.beginPath();
         ctx.rect(this.xpos, this.ypos, this.grid_size, this.grid_size);
         ctx.fillStyle = "#87FF00";
         ctx.fill();
-
-
     }
 }
 
 // 
-var direction = "";
-let s = new Snake(40, 20);
+x = Math.floor(Math.random() * (canvas.width / grid_size - 1)) + 1;
+y = Math.floor(Math.random() * (canvas.width / grid_size - 1)) + 1;
+let s = new Snake(x * grid_size, y * grid_size);
 s.draw(ctx);
 
 
@@ -107,29 +164,33 @@ const moveToDirection = function () {
 
 
 
-//hàm tạo 1 điểm thức ăn => tránh trường hợp thức ăn trùng với ô nằm trong thân rắn
-const makeFood = function () {
-    let x_food = Math.floor(Math.random() * (canvas.width / grid_size - 1)) + 1;
-    let y_food = Math.floor(Math.random() * (canvas.width / grid_size - 1)) + 1;
+let interval = setInterval(() => {
+    moveToDirection();
+}, 50);
 
-    if (snakeBody.some(function (item) {
-        return item[0] == x_food && item[1] == y_food;
-    })) {
-        makeFood();
-    } else {
-        ctx.beginPath();
-        ctx.rect(x_food * grid_size, y_food * grid_size, grid_size, grid_size);
-        ctx.fillStyle = "white";
-        ctx.fill()
-    }
+btn__load.addEventListener("click", function (e) {
+    window.location.reload();
+})
+
+
+const control_direction = function () {
+    document.addEventListener("keydown", function (event) {
+        // Kiểm tra xem phím nào được nhấ
+        let keyCode = event.key;
+
+        switch (keyCode) {
+            case 37:
+                if (direction == "right") {
+                    event.preventDefault();
+                }
+                break;
+
+            default:
+                break;
+        }
+    });
+
 }
 
+control_direction();
 
-makeFood()
-
-
-
-
-setInterval(() => {
-    moveToDirection();
-}, 100);
--- test.js
+++
@@ -1,4 +0,0 @@
-const array = [[1, 2], 3, 4, 5];
-
-console.log(array.includes([1,2])); // Output: true
-console.log(array.includes(6)); // Output: false
